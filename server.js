19a09b739925213e2445bd31e89c819c3503af69
rafael30121987 <rafael.nrosa@hotmail.com>
2024-05-13 23:12:15 -0300

    db connectionr
    install mysql, nodemon, dotenv. creating coneection with database

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const db = require("./config/db");
+const path = require("path");
+const redisClient = require("./config/redis")
+
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
+
+db.connect((err) => {
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+    console.log("Connected to MySQL DataBase...")
+})
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("<h1> test mayhem server</h1>")
+    res.render("index")
 })
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`,))
