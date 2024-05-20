9e29925d87745e36ad6445cd04975074b5162ffb
rafael30121987 <rafael.nrosa@hotmail.com>
2024-05-19 23:34:39 -0300

    some delet and redirects implementation

--- controller/views/index.js
+++ controller/views/index.js
@@ -1,7 +1,20 @@
 exports.getRegisterPage = (req, res) => {
+    if(req.cookies && req.cookies.token){
+        return res.redirect("/")
+    }
     res.render("auth/register")
 }
 
 exports.getLoginPage = (req, res) => {
+    if(req.cookies && req.cookies.token){
+        return res.redirect("/")
+    }
     res.render("auth/login")
+}
+
+exports.getLobbyPage = (req, res) => {
+    if(!req.cookies || !req.cookies.token){
+        return res.redirect("/login")
+    }
+    res.render("lobby");
 }
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,10 +1,12 @@
 const {Router} = require("express");
-const { getRegisterPage, getLoginPage } = require("../../controller/views");
+const { getRegisterPage, getLoginPage, getLobbyPage } = require("../../controller/views");
 
 const router = Router();
 
 router.get("/register", getRegisterPage)
 
 router.get("/login", getLoginPage)
+
+router.get("/", getLobbyPage)
 
 module.exports = router;
